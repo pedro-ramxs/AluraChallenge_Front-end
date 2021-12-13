276e198a3e2b6dc86695e7f20a165974513ad6d5
Pedro <pedroramoscarvalho2006@gmail.com>
2021-12-13 16:56:56 -0300

    Adicionando micro-interações e highlight.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,40 @@
     botaoPesquisa.addEventListener('click', () => {
         inputPesquisa.classList.toggle('cabecalho__search--ativo');
     });
+
+    const inputCor = document.querySelector('input[type=color]');
+
+    if (inputCor != null) {
+        const editor = document.querySelector('.editor');
+        editor.style.border = `32px solid ${inputCor.value}`;
+        inputCor.addEventListener('change', () => {
+            editor.style.border = `32px solid ${inputCor.value}`;
+        });
+    }
+    else{
+        const projetos = document.querySelectorAll('.editor');
+        projetos.forEach( projeto => {
+            projeto.style.border = "32px solid #6bd1ff";
+        });
+    }
+
+    const linguagem = document.querySelector('#linguagem');
+    const botaoHljs = document.querySelector('#btn-aplicaHljs');
+    const editor = document.querySelector('.editor');
+
+    if (botaoHljs != null){
+        botaoHljs.addEventListener('click', () => {
+            const codigo = editor.innerText;
+            editor.innerHTML = `<code class=" hljs language-${linguagem.value}" contenteditable="true" aria-label="Editor de código" spellcheck="false"></code`;
+            editor.querySelector('code').textContent = codigo;
+            hljs.highlightElement(editor.querySelector('code'));
+        });
+    }
+    else {
+        const projetos = document.querySelectorAll('.editor');
+        projetos.forEach( projeto => {
+            hljs.highlightElement(projeto.querySelector('code'));
+        });
+    }  
+
 })();
